9c55a1c55b3d0e3b1b217314fa9ef0eb5b0f294b
MacDev <macdev@MacDevs-MacBook-Air.local>
2024-03-21 07:06:33 -0400

    positional encoding complete

--- src/transformer/positional_embedding.py
+++ src/transformer/positional_embedding.py
@@ -1,21 +1,40 @@
 import torch
 from torch import nn
 
+
 class PositionalEncoding(nn.Module):
-    def __init__(self, seqLen: int, dmodel: int):
+    def __init__(self, seqLen: int, dmodel: int, dropout: float = 0.1):
+        super().__init__()
+
         self.seqLen = seqLen
         self.dmodel = dmodel
+        self.dropout = nn.Dropout(dropout)
 
         # Shape : (Batch, Position, Dim)
         # We can expand along batch, so focus on Position & Dim for now
+        pe = torch.zeros(self.seqLen, self.dmodel)
 
-        pos = torch.arange(0, self.seqLen, dtype=torch.float).unsqueeze(1)
-        dim = torch.arange(0, self.dmodel, dtype=torch.float).unsqueeze(0)
+        pos = torch.arange(0, self.seqLen, dtype=torch.float).unsqueeze(
+            1
+        )  # (seqLen, _)
 
-        pe = pos / (10000 ** (2*dim/self.dmodel)) 
-        print(pe)
+        div_term = 1000 ** (
+            torch.arange(0, self.dmodel, 2, dtype=torch.float).unsqueeze(0)
+            / self.dmodel
+        )
+
+        pe[:, 0::2] = torch.sin(pos / div_term)
+        pe[:, 1::2] = torch.cos(pos / div_term)
+
+        pe = pe.unsqueeze(0)  # (_, seqLen, dModel)
+
+        # Register as buffer - They will not be trained, but exist/persist in state_dict
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: torch.Tensor):
+        return self.dropout(x + (self.pe[:, : x.shape[1], :]).requires_grad_(False))
 
 
 if __name__ == "__main__":
-    a = PositionalEncoding(4, 3)
-
+    a = PositionalEncoding(12, 32)
+    print(a(torch.rand(12, 32)))
